3c27282a6e6c9c61c697fcd1c7fea1c9d73b7baa
Andrew Hinger <andrewjhinger@gmail.com>
2020-03-10 20:44:18 +0100

    toggle forms

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const messageInput = document.querySelector("#message-input");
+  const encrypted = btoa(messageInput.value);
+  const linkInput = document.querySelector("#link-input");
+
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
